3eba83215bfad444f984219a927caf44e1fc7a44
Rizwan <rizwan2000.rm@gmail.com>
2020-04-03 23:36:24 +0530

    Home Page Design

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express"),
   app = express();
 
+// dotenv
+require("dotenv").config();
+
 //EJS
 app.set("view engine", "ejs");
+
+// Static Files
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-
+app.listen(process.env.PORT, (req, res) => {
+  console.log("The QuickBites Server is Running.....");
+});
